c12a8691a0ea67fe805f254119f7777c6c29a3df
Taas Ekpaye <taasekpaye@outlook.fr>
2022-07-13 16:26:29 +0000

    typescript

---
+++ apis/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthenticationApi';
+export * from './ParamsApi';
+export * from './RequestApi';
+export * from './TicketApi';
+export * from './VacationApi';
